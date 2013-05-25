8de8bce3e984580ba87665d556569b71f04b73d9
Jason Sun <sunapi386@gmail.com>
2013-05-25 03:21:47 -0300

    Updated rangesort.cc, mostly tested preprocessing, testing search

--- rangesort.cc
+++ rangesort.cc
@@ -5,13 +5,13 @@ using namespace std;
 
 void map_add (map<int,int>* mymap, int key, int val) {
     mymap->insert(pair<int,int>(key,val));
-    if (DEBUG) cout << "added " << key << ", " << val << endl;
+    if (DEBUG) cout << "add " << key << ", " << val;
 }
 
-void map_update (map<int,int>* mymap, int key_to_update, int new_key, int val) {
-    mymap->erase (key_to_update);
+void map_update (map<int,int>* mymap, int old_key, int new_key, int val) {
+    mymap->erase ( mymap->find(old_key), mymap->upper_bound(val) );    // erasing by range [x,y)
     mymap->insert(pair<int,int>(new_key,val));
-    if (DEBUG) cout << "updated " << key_to_update << ", " << val << endl;
+    if (DEBUG) cout << "updated " << old_key << ", " << val << " to " << new_key << ", " << val;
 }
 
 
@@ -20,6 +20,7 @@ void read_and_build(map<int,int> &mymap) {
     cin >> pairs_to_read;
 
     for (int i = 0; i < pairs_to_read; ++i) {
+        if (DEBUG) cout << "\nLOOP " << i << ": ";
         int A, B;
         cin >> A >> B;
         if (A > B) {
@@ -30,7 +31,7 @@ void read_and_build(map<int,int> &mymap) {
 
         // Special case: initial
         if (mymap.size() == 0) {
-            if(DEBUG)  cout << "case 0 ";
+            if(DEBUG)  cout << "\t\t\t\tcase 0 ";
             map_add (&mymap, A, B);
             continue;
         }
@@ -38,7 +39,7 @@ void read_and_build(map<int,int> &mymap) {
         int a, b;
         a = it->first;
         b = it->second;
-        if(DEBUG)  cout << "\ta,b=[" << a << "," << b << "] A,B=[" << A << "," << B << "]" << endl;
+        if(DEBUG)  cout << "\ta,b=[" << a << "," << b << "] A,B=[" << A << "," << B << "]\t";
         /* 
         Variables A, B is the current range we are trying to add to list.
         Variables a, b is already in list. It is the closest range to A, B.
@@ -52,21 +53,21 @@ void read_and_build(map<int,int> &mymap) {
         6: Insert entire new range. A, B is right of a, b
         */
 
-        if (a < A && b < B && b > A) {
+        if (a <= A && b < B && b >= A) {
             if(DEBUG)  cout << "case 1 ";
             map_update (&mymap, a, a, B);
             continue;
         }
-        if (a > A && b > B && a < B) {
+        if (a <= A && b >= B && a <= B) {
             if(DEBUG)  cout << "case 2 ";
             continue;
         }
-        if (a > A && b > B && a < B) {   // map is read-only, so need to delete and re-add
+        if (a >= A && b >= B && a <= B) {   // map is read-only, so need to delete and re-add
             if(DEBUG)  cout << "case 3 ";
             map_update (&mymap, a, A, b);
             continue;
         }
-        if (a > A && b < B) {           // map is read-only, so need to delete and re-add
+        if (a >= A && b <= B) {           // map is read-only, so need to delete and re-add
             if(DEBUG)  cout << "case 4 ";
             map_update (&mymap, a, A, B);
             continue;
@@ -76,29 +77,43 @@ void read_and_build(map<int,int> &mymap) {
             map_add (&mymap, A, B);
             continue;
         }
-        if (B < a) {
+        if (B <= a) {
             if(DEBUG)  cout << "case 6 ";
             map_add (&mymap, A, B);
             continue;
         }
+        if(DEBUG) cout << " end LOOP " << i;
     } // for
 
 }
 
+// Print content
 void map_print (map<int,int>& m) {
-    cout << "print map" << endl;
-    // Print content
-    for (std::map<int,int>::iterator it=m.begin(); it!=m.end(); ++it)
-        std::cout << it->first << " --- " << it->second << '\n';
+    cout << "\nPRINTED MAP:" << endl;
+    for (map<int,int>::iterator it=m.begin(); it!=m.end(); ++it)
+        cout << it->first << " --- " << it->second << '\n';
 }
 
+bool map_is_in_range (map<int,int>& mymap, int x) {
+    map<int,int>::iterator it = mymap.upper_bound(x);
+    --it;
+    if (DEBUG) cout << "find " << x << " prev range " << it->first << ", " << it->second << endl;
+    return (x >= it->first && x <= it->second);
+}
 
 int main () {
     map<int,int> mymap;
-
     read_and_build(mymap);
-
     map_print (mymap);
+
+    while (true) {
+        int x = 0;
+        cin >> x;
+        cout << map_is_in_range(mymap, x) << endl;
+        if (x == 12345) break;
+    }
+    
+
 
     return 0;
 }
