7cd7f9dedf574fb898ac0d50e257c208099532db
Jason Sun <sunapi386@gmail.com>
2013-05-31 16:24:26 -0400

    Fixed merging, should good.

--- JavaUtils/src/LongestRunArray.java
+++ JavaUtils/src/LongestRunArray.java
@@ -11,9 +11,10 @@ import static com.sun.org.apache.xalan.internal.lib.ExsltMath.power;
  * Time: 7:27 PM
  */
 public class LongestRunArray {
-    public static final boolean DEBUG = false;
+    // To the Marker: set DEBUG flag to true to see debug statements. It is helpful.
+    public static final boolean DEBUG = true;
 
-    // Read m then m numbers from standard input, returning array A of size m
+    // Scans m then scans n=2^m numbers from standard input, returning array of size n
     public static final int[] build_array() {
         Scanner s = new Scanner(System.in);
         int m = s.nextInt();
@@ -58,7 +59,7 @@ public class LongestRunArray {
         return C;
     }
 
-    // Find longest run that contains elements from both A_left and A_right
+    // Find middle run that contains elements from both A_left and A_right
     // Note: there may not exist any runs with this property
     public static final int[] runner_middle(int[] A_left, int[] A_right) {
         // Done in O(n) by scanning to the left from end of A_left
@@ -68,49 +69,33 @@ public class LongestRunArray {
         // Last element in A_left must be less than first element in A_right
         if (A_left[A_left.length - 1] >= A_right[0]) return new int[0];
 
-        // Find left index starting from last element in A_left
+        // Find left index starting from last to first element in A_left
         int index_left = A_left.length - 1;
         for (int i = index_left; i > 0; i--) {
             if (A_left[i - 1] >= A_left[i]) break;
             index_left--;
         }
 
-        // Find right index starting from first element in A_right
+        // Find right index starting from first to last element in A_right
         int index_right = 0;
         for (int i = 0; i < A_right.length - 1; i++) {
             if (A_right[i] >= A_right[i + 1]) break;
             index_right++;
         }
 
-        assert (index_right < A_right.length);
         // Combine left and right and return it
-        int[] A_middle = new int[A_left.length - index_left + index_right];
-        A_middle = array_merge(array_split(A_left, index_left, A_left.length), array_split(A_right, 0, index_right + 1));
-        return A_middle;
+        int[] left = array_split(A_left, index_left, A_left.length);
+        int[] right = array_split(A_right, 0, index_right + 1);
+        return array_merge(left, right);
     }
 
-    // Merge: takes left, middle, and right arrays
-    // Preference always given to middle, and in the case middle is empty,
-    // the array with last element smallest is returned.
     public static final int[] runner_merge(int[] result_left, int[] result_middle, int[] result_right) {
-        int[] return_result;
-        int len_l = result_left.length;
-        int len_r = result_right.length;
-        int len_m = result_middle.length;
-        int[] result_result;
-        if (len_m >= len_l && len_m >= len_r) {
-            result_result = result_middle;
-        } else if (len_l > len_r) {
-            result_result = result_left;
-        } else if (len_l < len_r) {
-            result_result = result_right;
-        } else {
-            // In the case of "ties", it will not mater which one you return.
-            // This is because of the way the merge step works.
-            // - Dr. Stinson
 
-            result_result = result_right;
-        }
+        // Use leftmost long run convention
+
+        int[] result_result = result_right;
+        if (result_middle.length >= result_right.length) result_result = result_middle;
+        if (result_left.length >= result_middle.length) result_result = result_left;
 
         if (DEBUG) {
             System.out.print("Returning ");
@@ -123,35 +108,21 @@ public class LongestRunArray {
 
     // Divide and Conquer Algorithm
     public static final int[] runner(int[] given_A) {
-        // Return atomic arrays
+        // If atomic, return
         if (given_A.length == 1) return given_A;
 
-        // Otherwise divide work
+        // Otherwise divide and recurse left and right sub-arrays
         int half = (int) (given_A.length / 2);
-
-        // Make left and right sub-arrays
         int[] A_left = array_split(given_A, 0, half);
         int[] A_right = array_split(given_A, half, given_A.length);
+        int[] runner_left = runner(A_left);
+        int[] runner_right = runner(A_right);
 
-        // Recurse left and right array
-        int[] result_left = runner(A_left);
-        int[] result_right = runner(A_right);
+        // Check middle
+        int[] runner_middle = runner_middle(A_left, A_right);
 
-        // Check if middle has result
-        int[] result_middle = runner_middle(result_left, result_right);
-
-        if (DEBUG) {
-            System.out.println("+++");
-            System.out.print("L_");
-            array_print(result_left);
-            System.out.print("M_");
-            array_print(result_middle);
-            System.out.print("R_");
-            array_print(result_right);
-        }
-
-        // Merge who to return, left|middle|right arrays
-        int[] longest_runner = runner_merge(result_left, result_middle, result_right);
+        // Return longest runner
+        int[] longest_runner = runner_merge(runner_left, runner_middle, runner_right);
         return longest_runner;
     }
 
