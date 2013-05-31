3e1705e230f43395c436b347950e538c050a3348
Jason Sun <sunapi386@gmail.com>
2013-05-30 23:44:05 -0400

    Merge implemented. Runner not returning correct results, combining problem

--- JavaUtils/src/LongestRunArray.java
+++ JavaUtils/src/LongestRunArray.java
@@ -17,7 +17,7 @@ public class LongestRunArray {
     public static final int[] build_array() {
         Scanner s = new Scanner(System.in);
         int m = s.nextInt();
-        int n = (int) power(2,m);
+        int n = (int) power(2, m);
         int[] A = new int[n];
         for (int i = 0; i < n; i++) {
             A[i] = s.nextInt();
@@ -33,44 +33,108 @@ public class LongestRunArray {
 
     // Debugging: prints an array content
     public static final void array_print(int[] A) {
+        assert (A.length > 0);
         System.out.print("\nARRAY:");
         for (int i = 0; i < A.length; i++) {
             System.out.print(" " + A[i]);
         }
     }
 
-    // Helps to split array A into a smaller array from index low to high
+    // Split array A into a smaller array from index low to high
     public static final int[] array_split(int[] A, int low, int high) {
-        assert(high > low);
+        assert (high > low);
         int size = high - low;
         int[] smaller_A = new int[size];
-        for (int i = 0; i < smaller_A.length; i++) {
+        for (int i = 0; i < size; i++) {
             smaller_A[i] = A[low + i];
         }
         return smaller_A;
     }
 
-    // Divide and Conquer Algorithm
-    public static final int[] run(int[] given_A) {
-        int half = (int) (given_A.length * 0.5);
+    // Merges arrays A and B to C
+    public static final int[] array_merge(int[] A, int[] B) {
+        int[] C = new int[A.length + B.length];
+        for (int i = 0; i < A.length; i++) {
+            C[i] = A[i];
+        }
+        for (int i = 0; i < B.length; i++) {
+            C[i + A.length] = B[i];
+        }
+        return C;
+    }
 
+    // Find longest run that contains elements from both A_left and A_right
+    // Note: there may not exist any runs with this property
+    public static final int[] runner_merge(int[] A_left, int[] A_right) {
+        // Done in O(n) by scanning to the left from end of A_left
+        // and scanning to the right from beginning of A_right
+        assert (A_left.length >= 1 && A_right.length >= 1);
+
+        // Last element in A_left must be less than first element in A_right
+        if (A_left[A_left.length - 1] >= A_right[0]) return new int[0];
+
+        // Find left index starting from last element in A_left
+        int index_left = A_left.length - 1;
+        for (int i = index_left; i > 0; i--) {
+            if (A_left[i - 1] >= A_left[i]) break;
+            index_left--;
+        }
+
+        // Find right index starting from first element in A_right
+        int index_right = 0;
+        for (int i = 0; i < A_right.length - 1; i++) {
+            if (A_right[i] >= A_right[i + 1]) break;
+            index_right++;
+        }
+
+        assert (index_right < A_right.length);
+        // Combine left and right and return it
+        int[] A_middle = new int[A_left.length - index_left + index_right];
+        A_middle = array_merge(array_split(A_left, index_left, A_left.length), array_split(A_right, 0, index_right));
+        return A_middle;
+    }
+
+    // Divide and Conquer Algorithm
+    public static final int[] runner(int[] given_A) {
+        // Return atomic arrays
+        if (given_A.length == 1) return given_A;
+
+        // Otherwise divide
+        int half = (int) (given_A.length / 2);
+        // Make left and right sub-arrays
         int[] A_left = array_split(given_A, 0, half);
         int[] A_right = array_split(given_A, half, given_A.length);
-        if(DEBUG) {
-            array_print(A_left);
-            array_print(A_right);
+
+        // Recurse left and right array
+        int[] result_left = runner(A_left);
+        int[] result_right = runner(A_right);
+        int[] result_middle = runner_merge(result_left, result_right);
+        if (DEBUG) {
+            array_print(result_left);
+            array_print(result_middle);
+            array_print(result_right);
+        }
+
+        // PROBABLY WRONG
+        int len_l = result_left.length;
+        int len_r = result_right.length;
+        int len_m = result_middle.length;
+        if (len_m > len_l && len_m > len_r) {
+            return result_middle;
+        } else {
+            return given_A;
         }
 
 
-        int[] runner_Array = {0};
-        return runner_Array;
+
     }
 
     // Main program
     public static final void main(String[] args) {
         int[] A = build_array();
-        int[] longest_runner_array = run(A);
+        int[] longest_runner_array = runner(A);
 
         System.out.println("\nRESULT: " + longest_runner_array.length + " " + longest_runner_array[0]);
+        array_print(longest_runner_array);
     }
 }
